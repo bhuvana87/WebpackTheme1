8bac49dc97c38ef4ec6729b335ded3338cbff181
Bhuvaneshwari <bhuvaneswari.n@2adpro.com>
2017-03-21 16:27:01 +0530

    Webpack Sample - Loading JS wit babel-loader

---
+++ main.js
@@ -0,0 +1,7 @@
+require('./vendor/bootstrap/js/bootstrap.js');
+require('./js/jquery.easing.min.js');
+require('./js/jqBootstrapValidation.js');
+require('./js/contact_me.js');
+require('./js/agency.js');
+
+console.log("Js test");
