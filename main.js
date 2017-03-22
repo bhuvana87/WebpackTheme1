e8099952faad25be37f5149c86f13808f95d3293
Bhuvaneshwari <bhuvaneswari.n@2adpro.com>
2017-03-22 12:32:50 +0530

    CSS loaders

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+
+require("./vendor/bootstrap/css/bootstrap.css");
+require("./css/agency.css");
 require('./vendor/bootstrap/js/bootstrap.js');
 require('./js/jquery.easing.min.js');
 require('./js/jqBootstrapValidation.js');
--- webpack.config.js
+++ webpack.config.js
@@ -6,13 +6,21 @@ module.exports = {
     module: {
         loaders: [
             {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loaders: [
+                  'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+                  'image-webpack-loader?{progressive:true, optimizationLevel: 7, interlaced: false, pngquant:{quality: "65-90", speed: 4}}'
+                ]
+            },
+            { test: /\.css$/, loader: "style-loader!css-loader" },
+            {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 loader: 'babel-loader',
                 query: {
                     presets: ['es2015']
                 }
-            }
+            },
         ],
     }
 };
